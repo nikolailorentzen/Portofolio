afc7cc3d24a6c9f688bebff61a3a6b147bb6f92a
Nonsense Games <->
2024-05-14 16:43:33 +0200

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -25,24 +25,24 @@ const App = () => {
 
   return (
     
-    <div className="relative w-screen h-screen bg-slate-800 px-64 py-32 ">{/*overflow-hidden*/}
+    <div className="relative w-screen h-screen bg-slate-800 px-32 py-32 ">{/*overflow-hidden*/}
       <div className="pointer-events-none absolute inset-0 z-30" style={{background: `radial-gradient(600px at ${position.x}px ${position.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`}}></div>
       
-      <div className="flex flex-row align-center">
+      
         {/* <div className="h-40 w-40 relative bg-cover bg-no-repeat bg-pfp rounded-3xl shadow-inner"/>
         <div className="absolute h-40 w-40 rounded-3xl bg-sky-950 opacity-30"/> */}
-        <div className="flex flex-col">
-          <h1 className="text-4xl text-slate-200 tracking-tight pb-2">Nikolai Lorentzen</h1>{/* pil ikon */}
-          <h1 className="text-xl text-slate-400 tracking-tight">Frontend Developer</h1>
-          <h1 className="text-lg text-slate-500 tracking-tight">With a mind for beautiful designs.</h1>
+        <div className="flex">
+        <header className="flex flex-col lg:w-1/2 md:w-80">
+            <h1 className="text-4xl text-slate-200 tracking-tight pb-2">Nikolai Lorentzen</h1>{/* pil ikon */}
+            <h1 className="text-xl text-slate-400 tracking-tight">Frontend Developer</h1>
+            <h1 className="text-lg text-slate-500 tracking-tight">With a mind for beautiful designs.</h1>
+            <Nav/>
+          </header>
+          <main className="flex flex-col lg:w-1/2 md:w-80"> {/* last worked on*/}
+            <p className="text-lg text-slate-200 tracking-tight pb-2">ABOUT</p>
+            <p className="whitespace-normal text-slate-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi nemo culpa vitae ratione, omnis nulla inventore aperiam quos, eveniet, voluptatum odit repellat cum ea asperiores nostrum placeat iusto quod. Iusto quia modi earum quisquam, tempore placeat voluptatum soluta officiis qui odio error doloribus libero maxime commodi ea est reiciendis natus.</p>
+          </main>
         </div>
-      </div>
-      <Nav/>
-      
-      <p className="pt-16 text-slate-200">{selectedExperience?.toUpperCase()}</p>
-      <p className="pt-4 text-slate-400">Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br/>Tenetur quis corrupti ducimus qui rerum alias, aliquid illum, <br/>reiciendis, sed nostrum error voluptatem dicta eveniet! Eaque<br/> minus modi voluptas tempora dolorem!<br/><br/><span className="text-red-500">TODO: Legge til at man kan klikke på et av ikonene for å "aktivere" den react er først<br/>TODO: continue scroll animation when not hovered</span></p>
-      {/* <SocialMediaButtons/> */}
-
     </div>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,27 +1,42 @@
+import { useState } from "react";
 
 const Nav = () => {
 
-return (
-    <nav className="flex flex-col gap-2 mt-10 ">
+    const [selected, setSelected] = useState<string | null>("about");
 
-        <div className="group flex flex-row items-center text-slate-200">
-            <hr className="w-20 mr-5 bg-slate-200"></hr>
-            <h1 className="text-sm text-slate-200">ABOUT</h1>
-        </div>
-    
-        <div className="group flex flex-row items-center">
-            <hr className="w-8 mr-5 border-slate-500 group-hover:border-slate-200 group-hover:w-20"></hr>
-            <h1 className="text-sm text-slate-500 group-hover:text-slate-200">EXPERIENCE</h1>
-        </div>
+    const onClick = (e: React.MouseEvent<HTMLElement>) => { 
+        const dataTag = e.currentTarget.getAttribute("data-tag");
+        
+        if (dataTag) {
+            setSelected(dataTag);
+        }
+        else{
+            console.log("data-tag not found for HTMLElement")
+        }
+    };
 
-        <div className="group flex flex-row items-center text-slate-500">
-            <hr className="w-8 mr-5 border-slate-500 group-hover:border-slate-200 group-hover:w-20 transition duration-75"></hr>
-            <h1 className="text-sm text-slate-500 group-hover:text-slate-200">PROJECTS</h1>
-        </div>
+    return (
+        <nav>
+            <ul className="flex flex-col gap-2 mt-10">
+                <li data-tag="about" className="group flex flex-row items-center" onClick={onClick}>
+                    <hr className={`mr-5 ${selected === "about" ? 'border-slate-200 w-20' : 'border-slate-500 w-8 group-hover:border-slate-200 group-hover:w-20 all-transitions duration-500'}`}></hr>
+                    <h1 className={`text-sm ${selected === "about" ? 'text-slate-200' : 'text-slate-500 group-hover:text-slate-200'}`}>ABOUT</h1>
+                </li>
+        
+                <li data-tag="experience" className="group flex flex-row items-center" onClick={onClick}>
+                    <hr className={`mr-5 ${selected === "experience" ? 'border-slate-200 w-20' : 'border-slate-500 w-8 group-hover:border-slate-200 group-hover:w-20 all-transitions duration-500'}`}></hr>
+                    <h1 className={`text-sm ${selected === "experience" ? 'text-slate-200' : 'text-slate-500 group-hover:text-slate-200'}`}>EXPERIENCE</h1>
+                </li>
     
-    </nav>
+                <li data-tag="projects" className="group flex flex-row items-center text-slate-500" onClick={onClick}>
+                    <hr className={`mr-5 ${selected === "projects" ? 'border-slate-200 w-20' : 'border-slate-500 w-8 group-hover:border-slate-200 group-hover:w-20 all-transitions duration-500'}`}></hr>
+                    <h1 className={`text-sm ${selected === "projects" ? 'text-slate-200' : 'text-slate-500 group-hover:text-slate-200'}`}>PROJECTS</h1>
+                </li>
+            </ul>
+        </nav>
+        
+      );
+    };
     
-  );
-};
-
-export default Nav;
+    export default Nav;
+    
