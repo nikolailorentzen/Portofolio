74c7c4e70e720bd5c5f7da783850fd0a6d4b9023
paaulsens <95236172+paaulsens@users.noreply.github.com>
2024-06-07 00:19:42 +0200

    fixed compilation error

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,7 +14,7 @@ export const About = () => {
             
             <hr className="border-slate-500"/>
             <div className="flex flex-col gap-5 pt-5">
-                <p className="text-slate-300 text-md font-thin"><span className="text-slate-100 font-550 " >{t("helloThere")}</span>{t("introduction.paragraph1")}</p>
+                <p className="text-slate-300 text-md font-thin"><span className="text-slate-100 font-semibold" >{t("helloThere")}</span>{t("introduction.paragraph1")}</p>
                 <p className="text-slate-300 text-md font-thin">{t("introduction.paragraph2")}</p>
                 <p className="text-slate-300 text-md font-thin">{t("introduction.paragraph3")}</p>
                 <p className="text-slate-300 text-md font-thin">{t("introduction.paragraph4")}<span className="text-slate-200 font-semibold">{t("enjoyYourStay")}</span></p>
--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import i18n from '../localization/i18n';
 
 const LanguageSwitcher: React.FC = () => {
-  const { t } = useTranslation();
   const current = i18n.language
 
   const changeLanguage = (lng: string) => {
@@ -11,13 +9,10 @@ const LanguageSwitcher: React.FC = () => {
   };
 
   return (
-   
-        <div className='flex items-center gap-1 px-1'>
-          <div className={`bg-en-us bg-cover bg-center h-6 w-8 rounded-md filter ${current == "en" ? "grayscale-0" : "grayscale"} hover:grayscale-0 all-transitions duration-300`} onClick={() => changeLanguage('en')} />
-          <div className={`bg-no bg-cover bg-center h-6 w-8 rounded-md filter ${current == "no" ? "grayscale-0" : "grayscale"} hover:grayscale-0 all-transitions duration-300`} onClick={() => changeLanguage('no')} />
-        </div>
-      
-  
+      <div className='flex items-center gap-1 px-1'>
+        <div className={`bg-en-us bg-cover bg-center h-6 w-8 rounded-md filter ${current === "en" ? "grayscale-0" : "grayscale"} hover:grayscale-0 all-transitions duration-300`} onClick={() => changeLanguage('en')} />
+        <div className={`bg-no bg-cover bg-center h-6 w-8 rounded-md filter ${current === "no" ? "grayscale-0" : "grayscale"} hover:grayscale-0 all-transitions duration-300`} onClick={() => changeLanguage('no')} />
+      </div>
   );
 };
 
